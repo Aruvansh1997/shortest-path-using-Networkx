37b750a2f23c117abf81411bdac2ef0db512b133
Aruvansh1997 <aruvanshn@gmail.com>
2020-09-15 01:18:23 +0530

    Shortest path

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # shortest-path-using-Networkx
+
+1. Networkx used for finding the shortest path using djikstra.
+2. Node file contains nodes with their x y coordinates
+3. Edge file contains edges with their respective weights
+4. Using djikstra and only showing the shortest path in a graph.
+5. Visualization of this networkx is coupled with matplotlib.
+6. Dynamic edges are also added and removed to showcase the modularity of the
+   code.
+
+   (Highlighted path in red is the shortest path taken)
--- networkx.py
+++ networkx.py
@@ -4,7 +4,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+source=6
+target=7
 g = nx.Graph() 
 df=pd.read_csv(r'NodeList.csv')
 df_edge=pd.read_csv(r'EdgeList.csv')
@@ -16,7 +17,8 @@ pos = dict(zip(df.node,xy))
 
 # adding edges
 g.add_weighted_edges_from(edge)
-
+# to make the whole graph
+nx.draw_networkx(g,pos=pos)
 #dynamic addition of edges
 pos[6]=(18.4, 19.9)
 pos[7]=(20.2, 19.9)
@@ -24,9 +26,10 @@ g.add_edge(6, 1, weight=5)
 g.add_edge(6, 5, weight=5)
 g.add_edge(4, 7, weight=2)
 
-length,path =nx.single_source_dijkstra(g,source=6,target=7)
+length,path =nx.single_source_dijkstra(g,source=source,target=target)
 path_edges =set(zip(path,path[1:]))
 print(path,length)
+#highlighting the path of min distance
 nx.draw_networkx(g,pos,nodelist=path,node_color='r',edgelist=path_edges,edge_color='r',with_labels=True,)
 g.remove_node(6)
 g.remove_node(7)
