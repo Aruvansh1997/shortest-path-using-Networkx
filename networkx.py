d6d94e8fe6e748b8e510b3fe3b265e6f457de885
Aruvansh1997 <aruvanshn@gmail.com>
2020-09-15 01:00:54 +0530

    Shortest Path

---
+++ networkx.py
@@ -0,0 +1,33 @@
+#networkx for graph
+import networkx as nx
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+g = nx.Graph() 
+df=pd.read_csv(r'NodeList.csv')
+df_edge=pd.read_csv(r'EdgeList.csv')
+# weighted edge
+edge = list(zip(df_edge.node1,df_edge.node2,df_edge.wt))
+# node coordinates
+xy=list(zip(df.x,df.y))
+pos = dict(zip(df.node,xy))
+
+# adding edges
+g.add_weighted_edges_from(edge)
+
+#dynamic addition of edges
+pos[6]=(18.4, 19.9)
+pos[7]=(20.2, 19.9)
+g.add_edge(6, 1, weight=5)
+g.add_edge(6, 5, weight=5)
+g.add_edge(4, 7, weight=2)
+
+length,path =nx.single_source_dijkstra(g,source=6,target=7)
+path_edges =set(zip(path,path[1:]))
+print(path,length)
+nx.draw_networkx(g,pos,nodelist=path,node_color='r',edgelist=path_edges,edge_color='r',with_labels=True,)
+g.remove_node(6)
+g.remove_node(7)
+plt.show()
